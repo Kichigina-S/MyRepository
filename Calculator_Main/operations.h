3c0cbcd1b6a050178c62fd7a02eb599f93565cec
Светлана Кичигина <Kichigina>
2020-11-27 20:54:50 +0700

    New library with operations

---
+++ Calculator_Main/operations.h
@@ -0,0 +1,24 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+#endif // OPERATIONS_H
+
+double multyply(double first, double second)
+{
+    return first * second;
+}
+
+double sum(double first, double second)
+{
+    return first + second;
+}
+
+double minus(double first, double second)
+{
+    return first - second;
+}
+
+double devide(double first, double second)
+{
+    return first / second;
+}
