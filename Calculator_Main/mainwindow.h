76cda80e8f89ef9b70cd3834c207304463a2abc4
Светлана Кичигина <Kichigina>
2020-11-28 13:19:27 +0700

    Add metods in mainwindow.h

--- Calculator_Main/mainwindow.h
+++ Calculator_Main/mainwindow.h
@@ -17,6 +17,14 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+private slots:
+    void digits_numbers();
+    void on_pushButton_dot_clicked();
+    void operations();
+    void on_pushButton_clear_clicked();
+    void on_pushButton_equal_clicked();
+    void math_operations();
 };
 
 #endif // MAINWINDOW_H
